928b5d12c6f29e8660a33a09fb5f64775778c77f
Kas <kbberendsen@gmail.com>
2023-11-15 22:57:17 +0100

    small update to script

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function updateText() {
     const displayText = document.getElementById('displayText');
     const currentTime = new Date();
     const dayOfWeek = currentTime.getDay(); // 0 is Sunday, 1 is Monday, ...
+    const currentHour = currentTime.getHours();
   
     // Check if it's within the specific timeframes to display "no"
-    const currentHour = currentTime.getHours();
     if ((dayOfWeek === 2 && currentHour >= 9 && currentHour < 13) || // Tuesday: 9 AM to 1 PM
         (dayOfWeek === 4 && currentHour >= 13 && currentHour < 17)) { // Thursday: 1 PM to 5 PM
       displayText.textContent = 'Nee, ik heb nu geen weekend...';
